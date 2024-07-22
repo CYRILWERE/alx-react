db697f38b3bcc9c19008adb6b7d59cd42037a4c2
CYRILWERE <werecyril20@gmail.com>
2024-07-22 13:49:35 -0700

    task_2

---
+++ 0x00-Webpack/task_2/README.md
@@ -0,0 +1,10 @@
+# Webpack Configuration with CSS and Image Support
+
+This project demonstrates a Webpack setup that includes support for CSS and images, using jQuery and Lodash.
+
+## Installation
+
+1. Install dependencies:
+   ```sh
+   npm install
+
---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './js/dashboard_main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/',
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              disable: true, // Disable during development
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              webp: {
+                quality: 75,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  performance: {
+    maxAssetSize: 1000000,
+  },
+};
+

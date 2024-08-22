ad54587b0a52121f7fa77fc91276eb4c96eb7798
CYRILWERE <werecyril20@gmail.com>
2024-08-22 03:21:18 -0700

    task_5

---
+++ 0x07-react_redux_action_creator_normalizr/task_5/dashboard/src/actions/notificationActionCreators.js
@@ -0,0 +1,18 @@
+// task_5/dashboard/src/actions/notificationActionCreators.js
+
+import { MARK_AS_READ, SET_TYPE_FILTER } from './notificationActionTypes';
+
+export function markAsRead(index) {
+  return {
+    type: MARK_AS_READ,
+    index,
+  };
+}
+
+export function setNotificationFilter(filter) {
+  return {
+    type: SET_TYPE_FILTER,
+    filter,
+  };
+}
+
